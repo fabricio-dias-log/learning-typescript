fdecc7f1146b52a5d9efbff1442abe23b1d210d2
Fabricio Dias <fabricio270909@gmail.com>
2024-02-08 21:56:14 -0300

    Constraints in Generics

--- type-creation/index.js
+++ type-creation/index.js
@@ -6,3 +6,13 @@ function showData(arg) {
 console.log(showData(1));
 console.log(showData("test"));
 console.log(showData(true));
+// 2 - Constraint in generics
+function showProductName(obj) {
+    return `Product name is: ${obj.name}`;
+}
+const myObj = { name: "Plate", cor: "Black" };
+const otherProduct = { name: "Cigarettes", cor: "white" };
+const thirdObj = { price: 39.99, category: "clothes" };
+console.log(showProductName(myObj));
+console.log(showProductName(otherProduct));
+// console.log(showProductName(thirdObj)); // dont works because there aren't name property
--- type-creation/index.ts
+++ type-creation/index.ts
@@ -6,3 +6,16 @@ function showData<T>(arg: T): string {
 console.log(showData(1))
 console.log(showData("test"))
 console.log(showData(true))
+
+// 2 - Constraint in generics
+function showProductName<T extends {name: string}>(obj: T) {
+    return `Product name is: ${obj.name}`;
+}
+
+const myObj = {name: "Plate", cor: "Black"};
+const otherProduct = {name: "Cigarettes", cor: "white"};
+const thirdObj = {price: 39.99, category: "clothes"};
+
+console.log(showProductName(myObj));
+console.log(showProductName(otherProduct));
+// console.log(showProductName(thirdObj)); // dont works because there aren't name property
