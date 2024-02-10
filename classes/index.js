fa60d3e74f6d123ef21514602d5aa7d1171cc3b8
Fabricio Dias <fabricio270909@gmail.com>
2024-02-10 20:13:42 -0300

    Class fields

---
+++ classes/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// 1 - campos de classes
+class User {
+}
+const fabricio = new User();
+console.log(fabricio);
+fabricio.name = "Fabricio";
+console.log(fabricio);
---
+++ classes/index.ts
@@ -0,0 +1,11 @@
+// 1 - campos de classes
+class User {
+    name!: string
+    age!: number
+}
+
+const fabricio = new User();
+console.log(fabricio);
+
+fabricio.name = "Fabricio";
+console.log(fabricio);
