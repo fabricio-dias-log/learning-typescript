d854a33f019b2f8ec2dc9c7660f697fe96cef7a0
Fabricio Dias <fabricio270909@gmail.com>
2024-02-10 20:16:26 -0300

    Constructor

--- classes/index.js
+++ classes/index.js
@@ -6,3 +6,12 @@ const fabricio = new User();
 console.log(fabricio);
 fabricio.name = "Fabricio";
 console.log(fabricio);
+// 2 - Constructor
+class NewUser {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const jonas = new NewUser("Jonas", 15);
+console.log(jonas);
--- classes/index.ts
+++ classes/index.ts
@@ -9,3 +9,17 @@ console.log(fabricio);
 
 fabricio.name = "Fabricio";
 console.log(fabricio);
+
+// 2 - Constructor
+class NewUser {
+    name
+    age
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const jonas = new NewUser("Jonas", 15);
+console.log(jonas);
