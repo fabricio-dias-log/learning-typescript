ebadd996129f27291a625aef465dd6e8190b128f
Fabricio Dias <fabricio270909@gmail.com>
2024-02-10 23:48:59 -0300

    Import variable

--- modules/index.js
+++ modules/index.js
@@ -6,3 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // 1 - File import
 const greet_1 = __importDefault(require("./greet"));
 (0, greet_1.default)(); // in terminal use the command node index.js to use the function
+// 2 - Import variable
+const variables_1 = require("./variables");
+console.log(variables_1.x);
--- modules/index.ts
+++ modules/index.ts
@@ -1,3 +1,7 @@
 // 1 - File import
 import importGreet from "./greet";
 importGreet();// in terminal use the command node index.js to use the function
+
+// 2 - Import variable
+import {x} from "./variables";
+console.log(x)
