acf29e1c60f8db0d79b4573723e86ffefc73d381
Fabricio Dias <fabricio270909@gmail.com>
2024-02-10 23:17:35 -0300

    Parameters properties

--- classes/index.ts
+++ classes/index.ts
@@ -304,3 +304,25 @@ const secondItem = new Item(12, true)
 console.log(secondItem)
 console.log(secondItem.showFirst)
 console.log(typeof secondItem.showFirst)
+
+// 14 - Parameters properties
+class ParameterProperties {
+    constructor(public name: string, private readonly qty: number, private readonly price: number) {
+        this.name = name;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    get showQty() {
+        return `Total Qtd: ${this.qty}`;
+    }
+
+    get showPrice(){
+        return `The price is: ${this.price}`;
+    }
+}
+
+const newShirt = new ParameterProperties("Shirt", 5 , 19.99);
+console.log(newShirt.name);
+console.log(newShirt.showQty);
+console.log(newShirt.showPrice);
