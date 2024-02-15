2f5acb9ae64339b4ff6551123fa043b2f0d2e6ac
Fabricio Dias <fabricio270909hotmail.com>
2024-02-15 14:10:58 -0300

    Variables

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 function App() {
+  // Variables
+  const name: string = "Fabricio";
+  const age: number = 24;
+  const isWorking: boolean = true;
+
   return (
     <div className="App">
       <h1>TypeScript with React</h1>
+      <h2>Name: { name }</h2>
+      <p>Age: { age }</p>
+      {isWorking && (
+        <p>Working</p>
+      )}
     </div>
   );
 }
