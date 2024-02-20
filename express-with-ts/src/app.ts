2bff85e1073225337ceb1c402e0ae30e31c427c9
fabricio-intersoft <148587991+fabricio-intersoft@users.noreply.github.com>
2024-02-20 13:50:30 -0300

    Route Post

--- express-with-ts/src/app.ts
+++ express-with-ts/src/app.ts
@@ -8,6 +8,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+// (create a post route)
+app.use(express.json());
+
 app.listen(port, () => {
     console.log(`Server is running in port ${port}`);    
 })
@@ -15,5 +18,11 @@ app.listen(port, () => {
 // create a route
 app.get('/', (req, res) => {
     return res.send('Hello Express');
+})
+
+// create a post route
+app.post('/api/products', (req, res) => {
+    console.log(req.body);
+    return res.send('Product added');
 })
 
