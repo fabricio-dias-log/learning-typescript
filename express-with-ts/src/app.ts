e12bb37061f2fac8aba27c01acd475c01ff9a179
fabricio-intersoft <148587991+fabricio-intersoft@users.noreply.github.com>
2024-02-20 13:21:52 -0300

    Starting Express

--- express-with-ts/src/app.ts
+++ express-with-ts/src/app.ts
@@ -1,3 +1,19 @@
 // starting project
-console.log('Hello World');
-console.log('Express + TypeScript');
+// console.log('Hello World');
+// console.log('Express + TypeScript');
+
+// init express
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running in port ${port}`);    
+})
+
+// create a route
+app.get('/', (req, res) => {
+    return res.send('Hello Express');
+})
+
